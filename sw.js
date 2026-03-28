4aabf6fb6be0d16d3882e9140393f59509a1d371
Violet Brown <violetbrownpsych@gmail.com>
2026-03-27 19:14:14 -0500

    fixed cache issue

--- sw.js
+++ sw.js
@@ -3,8 +3,23 @@ var ASSETS = ['/', '/index.html', '/happyhours.csv'];
 
 self.addEventListener('install', function(e){
   e.waitUntil(caches.open(CACHE).then(function(c){ return c.addAll(ASSETS); }));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e){
+  e.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', function(e){
-  e.respondWith(caches.match(e.request).then(function(r){ return r || fetch(e.request); }));
+  e.respondWith(
+    caches.open(CACHE).then(function(cache){
+      return cache.match(e.request).then(function(cached){
+        var fetchPromise = fetch(e.request).then(function(response){
+          cache.put(e.request, response.clone());
+          return response;
+        });
+        return cached || fetchPromise;
+      });
+    })
+  );
 });
